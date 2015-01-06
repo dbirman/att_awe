90912d409a75c0d0853a47496470b29deed062a2
dbirman <danbirman@gmail.com>
2015-01-06 11:00:50 -0800

    Experiment Commit 1/6/14
    This is the version used to collect data on this day (real_faces2_agem
    is the current peripheral dataset)

---
+++ images/face_compare.m
@@ -0,0 +1,4 @@
+% This function displays, in one plot, random faces from a variety of face
+% database options. This allows you to quickly and accurately compare them
+% in equalized circumstances (all of the faces are displayed at equal
+% contrast 40%/60% std 50/256.
---
+++ images/face_grid.m
@@ -0,0 +1,3 @@
+% This function displays the contrast/noise grid for a random face from the
+% specified dataset, using the exact same procedure that cue_noisecon
+% employs.
