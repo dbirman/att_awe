fecb78c117b5aa717141b50ca05d9caeac6ecdff
dbirman <danbirman@gmail.com>
2015-01-21 10:29:40 -0800

    Major analysis update2

---
+++ analysis/generateBehFigures.m
@@ -0,0 +1,64 @@
+%% To start off, let's get files
+
+global analysis
+
+[analysis.datFolder, analysis.anFolder] = getSubjDataFolder;
+
+year = date;
+year = year(end-1:end);
+
+files = dir(sprintf('%s/%s*mat',analysis.datFolder,year));
+
+%% Loop over files, fixing them for analysis
+
+fixFiles(files);
+
+%% Loop over files and load them into expHolder
+
+expHolder = loadExp(files);
+
+%% Loop over exp files and export them to csv
+
+for ei = 1:length(expHolder)
+    try
+        exp2csv(expHolder{ei});
+    catch
+        disp(sprintf('Experiment file %i not generated...',ei));
+    end
+end
+
+%% The following functions use the most recent stimulus file
+
+load(fullfile(analysis.datFolder,files(end).name));
+
+%% Generate staircase graphs
+
+staircaseplots(stimulus);
+
+%% Generate discrimination functions
+
+% The idea here is just to compare the single and dual task performance on
+% the same graphs.
+plotting = gen_discFuncs(stimulus);
+
+%% normalize plotting
+
+[normplotting, plottingsd] = normPlotting(plotting);
+
+%% plot disc
+% gen_discPlots(plotting,stimulus,0);
+gen_discPlots(normplotting,plottingsd,stimulus);
+
+%% Generate peripheral performance
+
+peripheral = gen_perPerf(stimulus);
+gen_perPlots(peripheral,0);
+gen_perPlots(peripheral,1);
+
+%% Generate Performance Plots
+
+% The idea here is to have a plot that shows the dual task performance in
+% comparison with the single task performance for both gender and
+% contrast/noise at the same time.
+
+gen_perf(stimulus,normplotting,peripheral);
