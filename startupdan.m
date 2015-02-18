d89cb7e10bb408245c46d5d30574af34cea9502b
dbirman <danbirman@gmail.com>
2015-02-18 10:18:44 -0800

    Fixed a bug in startupdan

--- startupdan.m
+++ startupdan.m
@@ -39,14 +39,14 @@ end
 % run user specific matlab startup. This will look for a matlab
 % script like startupkenji to run, if it finds it on the path
 % will run
-username = getusername;
-userStartup = sprintf('startup%s',username);
-if exist(userStartup) == 2
-  disp(sprintf('(startup) Running user specific startup: %s',userStartup));
-  eval(userStartup);
-else
-  disp(sprintf('(startup) No user specific startup scrip found. If you need to, create one called: %s',userStartup));
-end  
+% username = getusername;
+% userStartup = sprintf('startup%s',username);
+% if exist(userStartup) == 2
+%   disp(sprintf('(startup) Running user specific startup: %s',userStartup));
+%   eval(userStartup);
+% else
+%   disp(sprintf('(startup) No user specific startup scrip found. If you need to, create one called: %s',userStartup));
+% end  
 
 addpath(genpath('~/proj/'))
 cd('~/proj/')
