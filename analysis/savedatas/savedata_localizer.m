fb937ebae77b8bbe968cbd10e2fa6ffebb6b5895
dbirman <danbirman@gmail.com>
2016-06-05 19:31:55 -0700

    New save data function

--- analysis/savedatas/savedata_localizer.m
+++ analysis/savedatas/savedata_localizer.m
@@ -19,11 +19,65 @@ view = newView();
 view = viewSet(view,'curGroup','Concatenation');
 view = viewSet(view,'curScan',1); % make sure scan # is correct
 
-%% Get the sort order from the _all_ analysis
+%% Get the mean timeseries using the reversed pRF
 view = loadAnalysis(view,sprintf('erAnal/%s','all')); % check analysis name!
-r2 = viewGet(view,'overlayData',view.curScan);
+ROIs = {'V1','V2','V3','V4','V3a','V3b','V7','LO1','LO2','MT'};
+pfxs = {'l','r'};
+allROIs = {};
+for ri = 1:length(ROIs)
+    for pi = 1:length(pfxs)
+        allROIs{end+1} = strcat(pfxs{pi},ROIs{ri});
+    end
+end
+analysis = view.analyses{1};
+rois = loadROITSeries(view,allROIs,view.curScan,view.curGroup,'keepNAN=true');
+
+tSeries = cell(1,length(rois)); % meaned tSeries
+r2s = cell(1,length(rois));
+rights = cell(1,length(rois));
+lefts = cell(1,length(rois));
+
+scanDims = viewGet(view,'scanDims');
+
+warning('Should check which overlay is which here, not just assume order');
+r2 = analysis.overlays(1).data{1};
+rois = getSortIndex(view,rois,r2);
+right = analysis.overlays(2).data{1};
+left = analysis.overlays(3).data{1};
+for ri = 1:length(rois)
+    r = rois{ri};
+    r.linearScanCoords = sub2ind(scanDims,r.scanCoords(1,:),r.scanCoords(2,:),r.scanCoords(3,:));
+    
+    cr2 = r2(r.linearScanCoords);    
+    rightO = right(r.linearScanCoords);
+    leftO = left(r.linearScanCoords);
+    rightO(isnan(rightO))=0;
+    leftO(isnan(leftO))=0;
+    
+    idxs = ~any(isnan(r.tSeries),2);
+    if any(~idxs)
+        keyboard
+    end
+%     tSeriesnoNaN = r.tSeries;
+%     tSeriesnoNaN(isnan(tSeriesnoNaN)) = 0;
+    
+    rightMean = (rightO*r.tSeries)/sum(rightO);
+    leftMean = (leftO*r.tSeries)/sum(leftO);
+    
+    r2s{ri} = cr2;
+    rights{ri} = rightO;
+    lefts{ri} = leftO;
+    
+    if strcmp(r.name(1),'l')
+        tSeries{ri} = rightMean;
+    else
+        tSeries{ri} = leftMean;
+    end
+end
 
 %% Pull Stimvols
+[lConvol, lConconds, ~] = getStimvol(view,'lCon','taskNum=1','phaseNum=2');
+[rConvol, rConconds, ~] = getStimvol(view,'rCon','taskNum=1','phaseNum=2');
 [convol, conconds, ~] = getStimvol(view,'contrast','taskNum=1','phaseNum=2');
 [cohvol, cohconds, ~] = getStimvol(view,'coherence','taskNum=1','phaseNum=2');
 [timvol, timconds, ~] = getStimvol(view,'timing','taskNum=1','phaseNum=2');
@@ -33,14 +87,15 @@ timdef = 5; % default of 2.5s stim
 basecon = 0.25;
 basecoh = 0;
 
+if ~isempty(lConvol)
+    warning('Write the code for lCoh/rCoh and Con');
+    keyboard
+end
+
 %% Convert to Long Form
 condata = volval2long(convol,conval);
 cohdata = volval2long(cohvol,cohval);
 timdata = volval2long(timvol,timval);
-
-%% Load occipital ROIs
-rois = loadROITSeries(view,{'V1'},view.curScan,view.curGroup,'keepNAN=true');
-rois = getSortIndex(view,rois,r2);
 
 %% Generate design
 % stimvol - basecon - newcon - basecoh - newcoh - timing
@@ -94,7 +149,11 @@ savefolder = '~/data/cohcon_localizer';
 fname = sprintf('%s_data.mat',cfolder);
 fname = fullfile(savefolder,fname);
 
-data.rois = rois;
+data.tSeries = tSeries;
+data.ROIs = allROIs;
+data.roi.r2 = r2s;
+data.roi.right = rights;
+data.roi.left = lefts;
 data.design = design;
 data.basecon = basecon;
 data.basecoh = basecoh;
